156384a3c62fe7458956a3d86cb7e6aeee1e3885
gim_razer <rgm79ms@gmail.com>
2019-03-13 12:02:36 +0800

    Added test scene

---
+++ Assets/Test/Test_FileIOUtility.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_FileIOUtility : MonoBehaviour {
+
+    public string imageSavingPath = "";
+
+
+	void Start () {
+
+        StartCoroutine(E_TakeScreenshot());
+
+    }
+	
+	void Update () {
+		
+	}
+
+    IEnumerator E_TakeScreenshot()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D tex2d_screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+        tex2d_screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        tex2d_screenshot.Apply();
+
+        Debug.Log("Photo taken!");
+
+        FileIOUtility.SaveImage(tex2d_screenshot,
+            imageSavingPath,
+            FileIOUtility.GenerateFileName("MyScreenshot", 12, FileIOUtility.FileExtension.PNG),
+            FileIOUtility.FileExtension.PNG);
+
+    }
+}
